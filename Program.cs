9491573f3a35b164e0b18bc30bd547f699510e9d
aniko <anogrigalashvili@gmail.com>
2019-12-25 14:20:52 +0400

    .

--- Program.cs
+++ Program.cs
@@ -21,54 +21,54 @@ namespace test
             var connectionString = configuration.GetConnectionString("DefaultConnection");
 
 
-            SqlConnection mySqlConnection =new SqlConnection(connectionString);
+            //SqlConnection mySqlConnection =new SqlConnection(connectionString);
 
-            string SelString = "SELECT Name, LastName " + "FROM Persons ";
-            SqlCommand mySqlCommand = mySqlConnection.CreateCommand();
-            mySqlCommand.CommandText = SelString;
-            SqlDataAdapter mySqlDataAdapter = new SqlDataAdapter();
-            mySqlDataAdapter.SelectCommand = mySqlCommand;
-            DataSet myDataSet = new DataSet();
-            mySqlConnection.Open();
-            string dataTableName = "Persons";
-            mySqlDataAdapter.Fill(myDataSet, dataTableName);
-            DataTable myDataTable = myDataSet.Tables[dataTableName];
-            foreach (DataRow myDataRow in myDataTable.Rows)
-            {
-                Console.WriteLine("Name = " + myDataRow["Name"]);
-                Console.WriteLine("LastName = " + myDataRow["LastName"]);
-            }
-            mySqlConnection.Close();
-
-
-
-            //Console.Write("Name=");
-            //var name = Console.ReadLine();
-            //Console.WriteLine();
-            //Console.Write("LastName=");
-            //var lastname = Console.ReadLine();
-            //DateTime BirthDate = new DateTime(1990, 1, 2);
-            //Console.WriteLine(BirthDate.ToString());
-
-            //using (var con = new SqlConnection(connectionString))
+            //string SelString = "SELECT Name, LastName " + "FROM Persons ";
+            //SqlCommand mySqlCommand = mySqlConnection.CreateCommand();
+            //mySqlCommand.CommandText = SelString;
+            //SqlDataAdapter mySqlDataAdapter = new SqlDataAdapter();
+            //mySqlDataAdapter.SelectCommand = mySqlCommand;
+            //DataSet myDataSet = new DataSet();
+            //mySqlConnection.Open();
+            //string dataTableName = "Persons";
+            //mySqlDataAdapter.Fill(myDataSet, dataTableName);
+            //DataTable myDataTable = myDataSet.Tables[dataTableName];
+            //foreach (DataRow myDataRow in myDataTable.Rows)
             //{
-            //    SqlCommand cmd = new SqlCommand("addperson", con);
-            //    cmd.Parameters.AddWithValue("@Name", name);
-            //    cmd.Parameters.AddWithValue("@LastName", lastname);
-            //    cmd.Parameters.AddWithValue("@BirthDate", BirthDate);
-
-            //    SqlParameter parm3 = new SqlParameter("@result", SqlDbType.NVarChar, size: 100);
-            //    parm3.Direction = ParameterDirection.Output;
-            //    cmd.Parameters.Add(parm3);
-
-            //    cmd.CommandType = CommandType.StoredProcedure;
-
-            //    con.Open();
-            //    cmd.ExecuteNonQuery();
-            //    con.Close();
-
-            //    Console.WriteLine(cmd.Parameters["@result"].Value);
+            //    Console.WriteLine("Name = " + myDataRow["Name"]);
+            //    Console.WriteLine("LastName = " + myDataRow["LastName"]);
             //}
+            //mySqlConnection.Close();
+
+
+
+            Console.Write("Name=");
+            var name = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("LastName=");
+            var lastname = Console.ReadLine();
+            DateTime BirthDate = new DateTime(1990, 1, 2);
+            Console.WriteLine(BirthDate.ToString());
+
+            using (var con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("addperson", con);
+                cmd.Parameters.AddWithValue("@Name", name);
+                cmd.Parameters.AddWithValue("@LastName", lastname);
+                cmd.Parameters.AddWithValue("@BirthDate", BirthDate);
+
+                SqlParameter parm3 = new SqlParameter("@result", SqlDbType.NVarChar, size: 100);
+                parm3.Direction = ParameterDirection.Output;
+                cmd.Parameters.Add(parm3);
+
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+                Console.WriteLine(cmd.Parameters["@result"].Value);
+            }
 
 
 
